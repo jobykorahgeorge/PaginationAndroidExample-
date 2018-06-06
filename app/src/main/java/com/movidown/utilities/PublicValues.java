8176264711b68555454e2c887a213b1e35f5d784
jobykorahgeorge <jobykorahgeorge@gmail.com>
2018-06-06 12:18:00 +0530

    Update PublicValues.java

--- app/src/main/java/com/movidown/utilities/PublicValues.java
+++ app/src/main/java/com/movidown/utilities/PublicValues.java
@@ -6,7 +6,7 @@ package com.movidown.utilities;
 
 public class PublicValues {
 
-    public static final String BASE_URL = "https://yifytorrent.to/api/v2/list_movies.json?limit=15&page=";
+    public static final String BASE_URL = "https://   /*its just an exampe api*//   api/v2/list_movies.json?limit=15&page=";
     public static final int NUM_ITEMS_IN_A_PAGE = 15; // number of items in a page
     public static final int PAGE_THRESHOLD = 5;
 
